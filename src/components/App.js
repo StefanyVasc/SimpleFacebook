227fc7bbeb86529a095dc72b948841110c3c69a2
Stefany Sá <stefany.vasc.sa@gmail.com.br>
2019-04-04 11:02:31 -0300

    termino do projeto

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+import Post from './Post';
+
+export default class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        name: 'Stefany Sá',
+        avatar: '',
+        time: 'há 3 min',
+        body:
+          'Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis',
+      },
+      {
+        id: 2,
+        name: 'Rocketseat',
+        avatar: '',
+        time: 'há 30 min',
+        body:
+          'Pellentesque, lacinia dui sed, ultricies sapien. Pellentesque orci lectus, consectetur vel posuere posuere, rutrum eu ipsum.',
+      },
+    ],
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div>
+        <Header />
+        <div className="post-container">
+          {posts && posts.map(post => <Post key={post.id} data={post} />)}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="header">
+    <h3>Facebook</h3>
+  </div>
+);
+
+export default Header;
